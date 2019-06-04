df8c2d552f7a569e1582d469e9b7f545a0eaa26d
goltz <tobias.goltz@integral-learning.de>
2019-06-04 11:24:23 +0200

    updated version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019052200;
+$plugin->version = 2019060400;
 $plugin->component = 'auth_mumie';
 $plugin->requires = 2015041700;
 $plugin->release = "v1.0";
