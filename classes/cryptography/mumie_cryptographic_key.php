cf42a43da347af694e431ba0eb01312db2796e4e
tobias <tobias.goltz@integral-learning.de>
2023-06-20 15:56:53 +0200

    Fixed code style

--- classes/cryptography/mumie_cryptographic_key.php
+++ classes/cryptography/mumie_cryptographic_key.php
@@ -1,5 +1,18 @@
 <?php
-
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 class mumie_cryptographic_key {
     const MUMIE_CRYPTOGRAPHIC_KEY_TABLE = "auth_mumie_cryptographic_key";
     private string $id;
@@ -25,66 +38,60 @@ class mumie_cryptographic_key {
         $DB->update_record(self::MUMIE_CRYPTOGRAPHIC_KEY_TABLE, ["name" => $this->name, "key" => $this->key, "id" => $this->id]);
     }
 
-    public static function getByName(string $name) : mumie_cryptographic_key | null{
+    public static function get_by_name(string $name) : mumie_cryptographic_key | null {
         global $DB;
         $record = $DB->get_record(self::MUMIE_CRYPTOGRAPHIC_KEY_TABLE, ["name" => $name]);
-        return self::fromRecord($record);
+        return self::from_record($record);
     }
 
-    private static function fromRecord($record) : mumie_cryptographic_key | null {
+    private static function from_record($record) : mumie_cryptographic_key | null {
         if (!$record) {
             return null;
         }
-        $cryptographic_key = new mumie_cryptographic_key($record->name, $record->key);
-        $cryptographic_key->setId($record->id);
-        return $cryptographic_key;
+        $cryptokey = new mumie_cryptographic_key($record->name, $record->key);
+        $cryptokey->set_id($record->id);
+        return $cryptokey;
     }
 
     /**
      * @return string
      */
-    public function getId() : string
-    {
+    public function get_id() : string {
         return $this->id;
     }
 
     /**
      * @param string $id
      */
-    public function setId(string $id) : void
-    {
+    public function set_id(string $id) : void {
         $this->id = $id;
     }
 
     /**
      * @return string
      */
-    public function getName() : string
-    {
+    public function get_name() : string {
         return $this->name;
     }
 
     /**
      * @param string $name
      */
-    public function setName(string $name) : void
-    {
+    public function set_name(string $name) : void {
         $this->name = $name;
     }
 
     /**
      * @return string
      */
-    public function getKey() : string
-    {
+    public function get_key() : string {
         return $this->key;
     }
 
     /**
      * @param string $key
      */
-    public function setKey(string $key) : void
-    {
+    public function set_key(string $key) : void {
         $this->key = $key;
     }
 
--- classes/cryptography/mumie_cryptography_service.php
+++ classes/cryptography/mumie_cryptography_service.php
@@ -1,17 +1,31 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+defined('MOODLE_INTERNAL') || die;
 
 require_once($CFG->dirroot . '/auth/mumie/classes/encryption/mumie_encryption_key.php');
 
-class mumie_cryptography_service
-{
+class mumie_cryptography_service {
     const PUBLIC_KEY_NAME = "public";
     const PRIVATE_KEY_NAME = "private";
     public static function get_public_key() : mumie_cryptographic_key | null {
-        return mumie_cryptographic_key::getByName(self::PUBLIC_KEY_NAME);
+        return mumie_cryptographic_key::get_by_name(self::PUBLIC_KEY_NAME);
     }
 
     public static function get_private_key() : mumie_cryptographic_key | null {
-        return mumie_cryptographic_key::getByName(self::PRIVATE_KEY_NAME);
+        return mumie_cryptographic_key::get_by_name(self::PRIVATE_KEY_NAME);
     }
 
     public static function ensure_key_pair_exist() {
@@ -32,10 +46,10 @@ class mumie_cryptography_service
         $res = openssl_pkey_new($config);
 
         openssl_pkey_export($res, $privatekey);
-        $pubKey = openssl_pkey_get_details($res);
-        $pubKey = $pubKey["key"];
+        $pubkey = openssl_pkey_get_details($res);
+        $pubkey = $pubkey["key"];
 
-        self::update_key_pair($privatekey, $pubKey);
+        self::update_key_pair($privatekey, $pubkey);
     }
 
     private static function update_key_pair(string $privatekey, string $publickey) : void {
@@ -52,9 +66,9 @@ class mumie_cryptography_service
     }
 
     private static function update_key(string $name, string $key) {
-        $cryptographickey = mumie_cryptographic_key::getByName($name);
+        $cryptographickey = mumie_cryptographic_key::get_by_name($name);
         if (!is_null($cryptographickey)) {
-            $cryptographickey->setKey($key);
+            $cryptographickey->set_key($key);
             $cryptographickey->update();
         } else {
             $cryptographickey = new mumie_cryptographic_key($name, $key);
